a92f65c2fccb4f0edcb7cf833711f0adbeed8b98
Md Nurul Islam <nurulcse09@gmail.com>
2022-12-19 10:27:29 +0600

    Mastering Fontend

---
+++ app/Http/Controllers/FontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FontController extends Controller
+{
+    public function home()
+    {
+        return view('home.index');
+    }
+
+    public function login()
+    {
+        return view('home.login');
+    }
+
+    public function signUp()
+    {
+        return view('home.signup');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FontController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[FontController::class,'home'])->name('/');
+Route::get('login',[FontController::class,'login'])->name('login');
+Route::get('signup',[FontController::class,'signUp'])->name('signup');
