d10dd0c2babe163aa772c6d7ff3fc8c4aacccf37
Md Nurul Islam <nurulcse09@gmail.com>
2022-12-23 16:42:30 +0600

    Updating Api Data

--- app/Http/Controllers/Assessment2.php
+++ app/Http/Controllers/Assessment2.php
@@ -55,7 +55,7 @@ class Assessment2 extends Controller
 
     public function deleteStudent($id)
     {
-        $data = Student::find($id);
+        $data = Student::findOrFail($id);
         $result = $data->delete();
 
         $success = 'Record Deleted successfully';
@@ -68,4 +68,30 @@ class Assessment2 extends Controller
             return response()->json(['message' => $fail], 502 );
         }
     }
+
+    //updateStudent
+    public function updateStudent(Request $request , $id)
+    {
+        $data = Student::findOrFail($id);
+
+        $data->user_id  = $request->user_id;
+        $data->name     = $request->name;
+        $data->email    = $request->email;
+        $data->contact  = $request->contact;
+
+        $result         = $data->save();
+
+        $success = 'Record Updated successfully';
+        $fail = 'Bad Gateway';
+
+        if($result) {
+            return response()->json(['message' => $success], 200);
+        }
+        else{
+            return response()->json(['message' => $fail], 502 );
+        }
+    }
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,20 @@ use App\Http\Controllers\Assessment2;
 
 
 //Get data
-
 Route::get('/view-student/{id?}',[Assessment2::class,'viewStudent']);
-
 // add Data
 Route::post('add-student',[Assessment2::class,'addStudent']);
-
 // delete Data
 Route::delete('delete-student/{id}',[Assessment2::class,'deleteStudent']);
+
+// update Data
+Route::put('update-student/{id}',[Assessment2::class,'updateStudent']);
+
+
+
+
+
+
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
